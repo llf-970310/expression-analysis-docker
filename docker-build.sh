6e63f6b15c7a89de1b2eece1b03ac3dcafdd5067
dylanchu <chdy.uuid@gmail.com>
2018-11-07 16:53:16 +0800

    update: docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -5,5 +5,4 @@ pwd
 tags=$(date +%Y%m%d-%H%M)
 docker build ./ -t exp-analysis:${tags}
 docker tag exp-analysis:${tags} exp-analysis:latest
-latest=`docker images -a|grep exp-analysis|grep latest|awk '{print $3}'`
-docker images -a|grep exp-analysis|grep -v ${latest}|awk '{print $3}'|xargs docker rmi
+docker images -a|grep exp-analysis|awk '{print $1":"$2}'|grep -v latest|grep -v ${tags}|xargs docker rmi
