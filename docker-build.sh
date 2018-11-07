30b8e99dcd9ac03152265a8026a80f20d3749e15
dylanchu <chdy.uuid@gmail.com>
2018-11-07 16:41:33 +0800

    update: docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -2,8 +2,8 @@
 
 cd `dirname $0`
 pwd
-latest=`docker images -a|grep exp-analysis|grep latest|awk '{print $3}'`
-docker images -a|grep exp-analysis|grep -v ${latest}|xargs docker rmi
 tags=$(date +%Y%m%d-%H%M)
 docker build ./ -t exp-analysis:${tags}
 docker tag exp-analysis:${tags} exp-analysis:latest
+latest=`docker images -a|grep exp-analysis|grep latest|awk '{print $3}'`
+docker images -a|grep exp-analysis|grep -v ${latest}|xargs docker rmi
