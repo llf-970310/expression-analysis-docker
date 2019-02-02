aeb482990e3f614daa29bf4f2690d4237a0c719b
dylanchu <chdy.uuid@gmail.com>
2019-02-02 18:32:34 +0800

    fix: priority queue for celery

--- expression/celery_tasks.py
+++ expression/celery_tasks.py
@@ -30,18 +30,7 @@ CELERY_QUEUES = (
     Queue('default', Exchange('default'), routing_key='default'),
 )  # consumer_arguments={'x-priority': 5}   数字越大，优先级越高
 
-# CELERY_ROUTES = {
-#     'app.tasks.analysis_main': {'queue': 'default', 'routing_key': 'default'},
-#     'app.tasks.analysis_main_12': {'queue': 'for_q_type12', 'routing_key': 'for_q_type12'},
-#     'app.tasks.analysis_main_3': {'queue': 'for_q_type3', 'routing_key': 'for_q_type3'},
-# }
 
-
-# CELERY_QUEUES = (
-#     Queue('high', Exchange('high'), routing_key='high'),
-#     Queue('normal', Exchange('normal'), routing_key='normal'),
-#     Queue('low', Exchange('low'), routing_key='low'),
-# )
 CELERY_DEFAULT_QUEUE = 'default'
 CELERY_DEFAULT_EXCHANGE = 'default'
 CELERY_DEFAULT_ROUTING_KEY = 'default'
@@ -52,72 +41,6 @@ CELERY_ROUTES = {
     'app.tasks.analysis_main_12': {'queue': 'for_q_type12'},
     'app.tasks.analysis_main': {'queue': 'default'},
 }
-
-
-@app.task
-def analysis_main(current_id, q_num):
-    # current_id = "5bcde8f30b9e037b1f67ba4e"
-    # q_num = "2"
-    logging.info("current_id: %s, q_num: %s" % (current_id, q_num))
-
-    mongo = db.Mongo()
-
-    feature = {}
-    score = 0
-    status = 'finished'
-    tr = None
-    # feature = get_feature(wf, q)
-    # score = get_score(q, feature)
-
-    q_info = mongo.get_question_info(current_id, q_num)
-    wf, q = mongo.get_wave_path_and_question(q_info)
-
-    tries = 0
-    while tries <= 3:
-        try:
-            # get api accounts
-            evl_account = mongo.get_evl_account()
-            rcg_account = mongo.get_rcg_account()
-            baidu_account = mongo.get_baidu_account()
-            config.XF_EVL_APP_ID = evl_account['appid']
-            config.XF_EVL_API_KEY = evl_account['key']
-            config.XF_RCG_APP_ID = rcg_account['appid']
-            config.XF_RCG_API_KEY = rcg_account['key']
-            config.BAIDU_APP_ID = baidu_account['appid']
-            config.BAIDU_API_KEY = baidu_account['api_key']
-            config.BAIDU_SECRET_KEY = baidu_account['secret_key']
-            logging.info('using EVL account: %s' % evl_account)
-            logging.info('using RCG account: %s' % rcg_account)
-            logging.info('using BAIDU account: %s' % baidu_account)
-
-            Q_type = q['q_type']
-            if Q_type == 1:
-                feature = analysis_features.analysis1(q_info['wav_temp_url'], q['text'], timeout=30)
-                score = analysis_scores.score1(feature)
-            elif Q_type == 2:
-                feature = analysis_features.analysis2(q_info['wav_temp_url'], q['wordbase'], timeout=30)
-                score = analysis_scores.score2(feature)
-            elif Q_type == 3:
-                feature = analysis_features.analysis3(q_info['wav_temp_url'], q['wordbase'], timeout=30)
-                score = analysis_scores.score3(feature)
-            else:
-                logging.error('Invalid question type: %s' % Q_type)
-            tries = 9999
-            status = 'finished'
-            tr = None
-            break
-        except Exception as e:
-            tr = traceback.format_exc()
-            print(tr)
-            logging.error('on retry %s: %s' % (tries, e))
-            status = 'error'
-            tries += 1
-
-    logging.info('Score: %s' % score)
-    mongo.save_result(current_id, q_num, q_info, feature, score, status=status, stack=tr)
-    os.system('rm %s' % q_info['wav_temp_url'])
-    os.system('rmdir %s > /dev/null 2>&1' % os.path.dirname(q_info['wav_temp_url']))
-    return status
 
 
 @app.task
