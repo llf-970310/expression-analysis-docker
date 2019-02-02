fecc7bd2e01eb5615176cd579e2489f5ca3381e6
dylanchu <chdy.uuid@gmail.com>
2019-02-02 16:49:58 +0800

    fix: using entrypoint.sh

--- expression/entrypoint.sh
+++ expression/entrypoint.sh
@@ -2,6 +2,6 @@
 
 echo executing entrypoint.sh ...
 celery worker -A celery_tasks.app -n worker_default -Q default --loglevel=info --concurrency=5 &
-celery worker -A celery_tasks.app -n worker_Qtype12 -Q for_question_type12 --loglevel=info --concurrency=12 &
-celery worker -A celery_tasks.app -n worker_Qtype3 -Q for_question_type3 --loglevel=info --concurrency=8 &
+celery worker -A celery_tasks.app -n worker_Qtype12 -Q for_q_type12 --loglevel=info --concurrency=12 &
+celery worker -A celery_tasks.app -n worker_Qtype3 -Q for_q_type3 --loglevel=info --concurrency=8 &
 celery flower -A celery_tasks.app --address=0.0.0.0 --port=50080
