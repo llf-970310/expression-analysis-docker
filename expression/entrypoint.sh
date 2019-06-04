356659d64b46e5d974f113c0572bb092bc12fc12
gyue15 <786330297@qq.com>
2019-06-04 22:45:35 +0800

    feat: 修改队列名称

--- expression/entrypoint.sh
+++ expression/entrypoint.sh
@@ -9,9 +9,9 @@ celery worker -P gevent -A celery_tasks.app -n Qtype3_worker3@%h -Q q_type3 -l i
 celery worker -P gevent -A celery_tasks.app -n Qtype12_worker1@%h -Q q_type12 -l info -c 10 &
 celery worker -P gevent -A celery_tasks.app -n Qtype12_worker2@%h -Q q_type12 -l info -c 10 &
 celery worker -P gevent -A celery_tasks.app -n Qtype12_worker3@%h -Q q_type12 -l info -c 10 &
-celery worker -P gevent -A celery_tasks.app -n Qpre_worker1@%h -Q q_for_test -l info -c 10 &
-celery worker -P gevent -A celery_tasks.app -n Qpre_worker2@%h -Q q_for_test -l info -c 10 &
-celery worker -P gevent -A celery_tasks.app -n Qpre_worker3@%h -Q q_for_test -l info -c 10
+celery worker -P gevent -A celery_tasks.app -n Qpre_worker1@%h -Q q_pre_test -l info -c 10 &
+celery worker -P gevent -A celery_tasks.app -n Qpre_worker2@%h -Q q_pre_test -l info -c 10 &
+celery worker -P gevent -A celery_tasks.app -n Qpre_worker3@%h -Q q_pre_test -l info -c 10
 # celery multi start 2 -P gevent -A celery_tasks.app -n worker_Qtype3@%h -Q for_q_type3 -l info -c:1-2 15 &
 # celery multi start 2 -P gevent -A celery_tasks.app -n worker_Qtype12@%h -Q for_q_type12 -l info -c:1-2 15 &
 # celery worker -A celery_tasks.app -n worker_Qtype123 -Q for_q_type3,for_q_type12 --loglevel=info --concurrency=10 &
