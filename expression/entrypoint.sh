eefbb3aed0c018473c321502ea97e424ccee351e
dylanchu <chdy.uuid@gmail.com>
2019-11-20 19:30:40 +0800

    update: entrypoint.sh

--- expression/entrypoint.sh
+++ expression/entrypoint.sh
@@ -8,9 +8,7 @@ echo executing entrypoint.sh ...
 # celery multi start 2 -P gevent -A celery_tasks.app -n worker_Type12@%h -Q q_type12 -l info -c:1-2 15 &
 celery worker -P gevent -A celery_tasks.app -n %h_Qt-Pre_p1 -Q q_pre_test -l info -c 15 &
 celery worker -P gevent -A celery_tasks.app -n %h_Qt-Pre_p2 -Q q_pre_test -l info -c 15 &
-celery worker -P gevent -A celery_tasks.app -n %h_Qt-12_p1 -Q q_type12 -l info -c 15 &
-celery worker -P gevent -A celery_tasks.app -n %h_Qt-12_p2 -Q q_type12 -l info -c 15 &
-celery worker -P gevent -A celery_tasks.app -n %h_Qt-3_p1 -Q q_type3 -l info -c 15 &
-celery worker -P gevent -A celery_tasks.app -n %h_Qt-3_p2 -Q q_type3 -l info -c 15
+celery worker -P gevent -A celery_tasks.app -n %h_Qt-12_p1 -Q q_type12 -l info -c 30 &
+celery worker -P gevent -A celery_tasks.app -n %h_Qt-3_p1 -Q q_type3 -l info -c 30
 
 # celery flower -A celery_tasks.app --address=0.0.0.0 --port=50080
