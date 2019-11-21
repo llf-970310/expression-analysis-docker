fcb9cc386e667dfec0e131e2fd7f5dc7b796580e
dylanchu <chdy.uuid@gmail.com>
2019-11-22 03:10:41 +0800

    build: 调整worker并发数

--- expression/entrypoint.sh
+++ expression/entrypoint.sh
@@ -6,11 +6,10 @@ echo executing entrypoint.sh ...
 # celery worker -A celery_tasks.app -n worker_Type123 -Q q_type3,q_type12 --loglevel=info --concurrency=10 &
 # celery multi start 2 -P gevent -A celery_tasks.app -n worker_Type3@%h -Q q_type3 -l info -c:1-2 15 &
 # celery multi start 2 -P gevent -A celery_tasks.app -n worker_Type12@%h -Q q_type12 -l info -c:1-2 15 &
-celery worker -P gevent -A celery_tasks.app -n %h_Qt-Pre_p1 -Q q_pre_test -l info -c 15 &
-celery worker -P gevent -A celery_tasks.app -n %h_Qt-Pre_p2 -Q q_pre_test -l info -c 15 &
-celery worker -P gevent -A celery_tasks.app -n %h_Qt-12_p1 -Q q_type12 -l info -c 15 &
-celery worker -P gevent -A celery_tasks.app -n %h_Qt-12_p2 -Q q_type12 -l info -c 15 &
-celery worker -P gevent -A celery_tasks.app -n %h_Qt-3_p1 -Q q_type3 -l info -c 15 &
-celery worker -P gevent -A celery_tasks.app -n %h_Qt-3_p2 -Q q_type3 -l info -c 15
+celery worker -P gevent -A celery_tasks.app -n %h_Qt-Pre_p1 -Q q_pre_test -l info -c 20 &
+celery worker -P gevent -A celery_tasks.app -n %h_Qt-12_p1 -Q q_type12 -l info -c 12 &
+celery worker -P gevent -A celery_tasks.app -n %h_Qt-12_p2 -Q q_type12 -l info -c 13 &
+celery worker -P gevent -A celery_tasks.app -n %h_Qt-3_p1 -Q q_type3 -l info -c 12 &
+celery worker -P gevent -A celery_tasks.app -n %h_Qt-3_p2 -Q q_type3 -l info -c 13
 
 # celery flower -A celery_tasks.app --address=0.0.0.0 --port=50080
