21e84396ba8924fd2425f0cfa795ad61221cfdfe
dylanchu <chdy.uuid@gmail.com>
2019-11-22 15:58:02 +0800

    build:尝试调整题型3 worker数量

--- expression/entrypoint.sh
+++ expression/entrypoint.sh
@@ -11,12 +11,35 @@ echo executing entrypoint.sh ...
 celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-Pre_p1" -Q q_pre_test -l info -c 20 &
 celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-12_p1" -Q q_type12 -l info -c 12 &
 celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-12_p2" -Q q_type12 -l info -c 13 &
-celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p1" -Q q_type3 -l info -c 4 &
-celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p2" -Q q_type3 -l info -c 4 &
-celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p3" -Q q_type3 -l info -c 4 &
-celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p4" -Q q_type3 -l info -c 4 &
-celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p5" -Q q_type3 -l info -c 4 &
-celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p6" -Q q_type3 -l info -c 4 &
-celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p7" -Q q_type3 -l info -c 6
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p1" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p2" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p3" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p4" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p5" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p6" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p7" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p8" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p9" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p10" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p11" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p12" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p13" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p14" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p15" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p16" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p17" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p18" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p19" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p20" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p21" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p22" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p23" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p24" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p25" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p26" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p27" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p28" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p29" -Q q_type3 -l info -c 1 &
+celery worker -P gevent -A celery_tasks.app -n $this_ip"-Qt-3_p30" -Q q_type3 -l info -c 1
 
 # celery flower -A celery_tasks.app --address=0.0.0.0 --port=50080
